885e12a5509f1c0a940bd003352633e46b379c5d
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-03-02 17:22:55 +0100

    add edge cases to listItem test

--- Client/src/views/children/ChildrenList/List/Item/ListViewItem.spec.js
+++ Client/src/views/children/ChildrenList/List/Item/ListViewItem.spec.js
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+// import { renderer } from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
 import ListViewItem from ".";
 import { children } from "../../../../../assets/mocks/children";
@@ -20,10 +21,22 @@ test("ListViewItem should be rendered in the list", () => {
   expect(itemInstance).toBeInTheDocument();
 });
 
-test("ListViewItem should get link to single child view", () => {
+test("ListViewItem should get link to single child view (first on list)", () => {
   const fullName = setup(0).getByText(/Amy Raven/);
   expect(fullName).toBeInTheDocument();
   expect(fullName.closest('a')).toHaveAttribute('href', '/children/6')
+});
+
+test("ListViewItem should get link to single child view (middle)", () => {
+  const fullName = setup(2).getByText(/John Doe/);
+  expect(fullName).toBeInTheDocument();
+  expect(fullName.closest('a')).toHaveAttribute('href', '/children/3')
+});
+
+test("ListViewItem should get link to single child view (last on list)", () => {
+  const fullName = setup(children.length-1).getByText(/Sophie Moon/);
+  expect(fullName).toBeInTheDocument();
+  expect(fullName.closest('a')).toHaveAttribute('href', '/children/5')
 });
 
 test("ListViewItem should contain group name", () => {
@@ -32,13 +45,7 @@ test("ListViewItem should contain group name", () => {
   expect(groupName.closest('p').innerHTML).toContain('Monkeys')
 });
 
-test("ListViewItem should get link to single child view", () => {
-  const fullName = setup(2).getByText(/John Doe/);
-  expect(fullName).toBeInTheDocument();
-  expect(fullName.closest('a')).toHaveAttribute('href', '/children/3')
-});
-
-test("ListViewItem should contain group name", () => {
+test("ListViewItem should contain group name (another example)", () => {
   const groupName = setup(2).getByText(/Group:/);
   expect(groupName).toBeInTheDocument();
   expect(groupName.closest('p').innerHTML).toContain('Crocodiles')
