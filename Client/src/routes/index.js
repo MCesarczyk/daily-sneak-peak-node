c361c48390251d2e1075470201f2b08edb353be3
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-03-05 23:21:20 +0100

    redirect all invalid paths to children

--- Client/src/routes/index.js
+++ Client/src/routes/index.js
@@ -3,11 +3,11 @@ import ChildrenList from "../views/children/ChildrenList";
 import ChildData from "../views/child/ChildData";
 
 const Paths = () => (
-            <Routes>
-                <Route path='/' element={<Navigate replace to='/children' />} />
-                <Route path='/children' element={<ChildrenList />} />
-                <Route path='/children/:id' element={<ChildData />} />
-            </Routes>
+    <Routes>
+        <Route path='*' element={<Navigate replace to='/children' />} />
+        <Route path='/children' element={<ChildrenList />} />
+        <Route path='/children/:id' element={<ChildData />} />
+    </Routes>
 );
 
 export default Paths;
