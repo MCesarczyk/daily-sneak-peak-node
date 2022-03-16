7c593ad964417706138082522b8b7a0a1f439dc4
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-03-16 23:08:54 +0100

    fix API links on backend

--- Frontend/src/assets/links.js
+++ Frontend/src/assets/links.js
@@ -1,5 +1,7 @@
-export const TITLE_URL = "/api/title";
-export const CHILDREN_LIST_URL = "/api/get/children";
-export const CHILD_POST_URL = "/api/post/childtodb";
-export const CHILD_UPDATE_URL = "/api/put/children";
-export const CHILD_DELETE_URL = "/api/delete/children";
+const apiUrl = 'http://164.92.135.59:5000'
+
+export const TITLE_URL = `${apiUrl}/api/title`;
+export const CHILDREN_LIST_URL = `${apiUrl}/api/get/children`;
+export const CHILD_POST_URL = `${apiUrl}/api/post/childtodb`;
+export const CHILD_UPDATE_URL = `${apiUrl}/api/put/children`;
+export const CHILD_DELETE_URL = `${apiUrl}/api/delete/children`;
