43521197e3bbf4bb1ace6db6b450080545333c87
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-04-14 17:34:52 +0200

    removed obsolete debugging log

--- Frontend/src/views/child/ChildData/Tile/Image/index.js
+++ Frontend/src/views/child/ChildData/Tile/Image/index.js
@@ -2,15 +2,12 @@ import { NoPicture } from "./NoPicture";
 import { ImageOuterWrapper, ImageWrapper } from "./styled";
 import Portrait from "./Portrait";
 
-const Image = ({ child }) => {
-  console.log(child?.avatarUrl);
-
-  return(
+const Image = ({ child }) => (
   <ImageOuterWrapper>
     <ImageWrapper>
       {(child?.avatarUrl === '' || child?.avatarUrl === undefined) ? <NoPicture /> : <Portrait />}
     </ImageWrapper>
   </ImageOuterWrapper>
-)};
+);
 
 export default Image;
